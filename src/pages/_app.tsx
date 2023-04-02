1696d62eebbf79cc25f633e5c93e199cc9bea7c7
mbuguaTheDev <opencubetechnologies@gmail.com>
2023-04-02 17:37:36 +0300

    intial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "../theme/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
---
+++ src/theme/theme.ts
@@ -0,0 +1,269 @@
+import { createTheme } from "@mui/material";
+import { Barlow } from "next/font/google";
+
+const barlow = Barlow({
+  weight: ["400", "500", "600", "700"],
+  style: "normal",
+  subsets: ["latin"],
+});
+
+export const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 640,
+      md: 1024,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        contained: {
+          "&:hover": {
+            backgroundColor: "#ffbb3b",
+          },
+        },
+        sizeSmall: {
+          padding: "6px 16px",
+        },
+        sizeMedium: {
+          padding: "8px 20px",
+        },
+        sizeLarge: {
+          padding: "11px 24px",
+        },
+        textSizeSmall: {
+          padding: "7px 12px",
+        },
+        textSizeMedium: {
+          padding: "9px 16px",
+        },
+        textSizeLarge: {
+          padding: "12px 16px",
+        },
+      },
+    },
+    MuiCardContent: {
+      styleOverrides: {
+        root: {
+          padding: "20px",
+          "&:last-child": {
+            paddingBottom: 20,
+          },
+        },
+      },
+    },
+    MuiCardHeader: {
+      defaultProps: {
+        titleTypographyProps: {
+          variant: "h5",
+        },
+        subheaderTypographyProps: {
+          variant: "body2",
+        },
+      },
+      styleOverrides: {
+        root: {
+          padding: "20px 24px 0",
+        },
+      },
+    },
+    MuiCssBaseline: {
+      styleOverrides: {
+        "*": {
+          boxSizing: "border-box",
+          margin: 0,
+          padding: 0,
+        },
+        html: {
+          MozOsxFontSmoothing: "grayscale",
+          WebkitFontSmoothing: "antialiased",
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100%",
+          width: "100%",
+        },
+        body: {
+          display: "flex",
+          flex: "1 1 auto",
+          flexDirection: "column",
+          minHeight: "100%",
+          width: "100%",
+          margin: 0,
+          padding: 0,
+        },
+        "#__next": {
+          display: "flex",
+          flex: "1 1 auto",
+          flexDirection: "column",
+          height: "100%",
+          width: "100%",
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        notchedOutline: {
+          borderColor: "#E6E8F0",
+        },
+      },
+    },
+  },
+  palette: {
+    action: {
+      active: "#6B7280",
+      focus: "rgba(55, 65, 81, 0.12)",
+      hover: "rgba(55, 65, 81, 0.04)",
+      selected: "rgba(55, 65, 81, 0.08)",
+      disabledBackground: "rgba(55, 65, 81, 0.12)",
+      disabled: "rgba(55, 65, 81, 0.26)",
+    },
+    background: {
+      default: "#F9FAFC",
+      paper: "#FFFFFF",
+    },
+
+    primary: {
+      main: "#08B592",
+      light: "#39c3a7",
+      dark: "#057e66",
+      contrastText: "#000000",
+    },
+    secondary: {
+      main: "#FFAA0A",
+      light: "#ffbb3b",
+      dark: "#b27607",
+      contrastText: "#000000",
+    },
+    success: {
+      main: "#08B592",
+      light: "#39c3a7",
+      dark: "#057e66",
+      contrastText: "#000000",
+    },
+    info: {
+      main: "#2196F3",
+      light: "#64B6F7",
+      dark: "#0B79D0",
+      contrastText: "#FFFFFF",
+    },
+    warning: {
+      main: "#f5b33b",
+      light: "#f7c262",
+      dark: "#ab7d29",
+      contrastText: "#FFFFFF",
+    },
+    error: {
+      main: "#d90707",
+      light: "#e03838",
+      dark: "#970404",
+      contrastText: "#FFFFFF",
+    },
+    text: {
+      primary: "#193042",
+      secondary: "#65748B",
+      disabled: "rgba(55, 65, 81, 0.48)",
+    },
+  },
+  shape: {
+    borderRadius: 50,
+  },
+  shadows: [
+    "none",
+    "0px 1px 1px rgba(100, 116, 139, 0.06), 0px 1px 2px rgba(100, 116, 139, 0.1)",
+    "0px 1px 2px rgba(100, 116, 139, 0.12)",
+    "0px 1px 4px rgba(100, 116, 139, 0.12)",
+    "0px 1px 5px rgba(100, 116, 139, 0.12)",
+    "0px 1px 6px rgba(100, 116, 139, 0.12)",
+    "0px 2px 6px rgba(100, 116, 139, 0.12)",
+    "0px 3px 6px rgba(100, 116, 139, 0.12)",
+    "0px 2px 4px rgba(31, 41, 55, 0.06), 0px 4px 6px rgba(100, 116, 139, 0.12)",
+    "0px 5px 12px rgba(100, 116, 139, 0.12)",
+    "0px 5px 14px rgba(100, 116, 139, 0.12)",
+    "0px 5px 15px rgba(100, 116, 139, 0.12)",
+    "0px 6px 15px rgba(100, 116, 139, 0.12)",
+    "0px 7px 15px rgba(100, 116, 139, 0.12)",
+    "0px 8px 15px rgba(100, 116, 139, 0.12)",
+    "0px 9px 15px rgba(100, 116, 139, 0.12)",
+    "0px 10px 15px rgba(100, 116, 139, 0.12)",
+    "0px 12px 22px -8px rgba(100, 116, 139, 0.25)",
+    "0px 13px 22px -8px rgba(100, 116, 139, 0.25)",
+    "0px 14px 24px -8px rgba(100, 116, 139, 0.25)",
+    "0px 10px 10px rgba(31, 41, 55, 0.04), 0px 20px 25px rgba(31, 41, 55, 0.1)",
+    "0px 25px 50px rgba(100, 116, 139, 0.25)",
+    "0px 25px 50px rgba(100, 116, 139, 0.25)",
+    "0px 25px 50px rgba(100, 116, 139, 0.25)",
+    "0px 25px 50px rgba(100, 116, 139, 0.25)",
+  ],
+  typography: {
+    button: {
+      fontWeight: 600,
+      color: "#193042",
+    },
+    fontFamily: barlow.style.fontFamily,
+    body1: {
+      fontSize: "1rem",
+      fontWeight: 400,
+      lineHeight: 1.5,
+    },
+    body2: {
+      fontSize: "0.875rem",
+      fontWeight: 400,
+      lineHeight: 1.2,
+    },
+    subtitle1: {
+      fontSize: "1rem",
+      fontWeight: 500,
+      lineHeight: 1.75,
+    },
+    subtitle2: {
+      fontSize: "0.875rem",
+      fontWeight: 500,
+      lineHeight: 1.57,
+    },
+    overline: {
+      fontSize: "0.75rem",
+      fontWeight: 600,
+      letterSpacing: "0.5px",
+      lineHeight: 2.5,
+      textTransform: "uppercase",
+    },
+    caption: {
+      fontSize: "0.75rem",
+      fontWeight: 400,
+      lineHeight: 1.66,
+    },
+    h1: {
+      fontWeight: 700,
+      fontSize: "3.5rem",
+      lineHeight: 1.375,
+    },
+    h2: {
+      fontWeight: 700,
+      fontSize: "3rem",
+      lineHeight: 1.375,
+    },
+    h3: {
+      fontWeight: 700,
+      fontSize: "2.25rem",
+      lineHeight: 1.375,
+    },
+    h4: {
+      fontWeight: 700,
+      fontSize: "2rem",
+      lineHeight: 1.375,
+    },
+    h5: {
+      fontWeight: 600,
+      fontSize: "1.25rem",
+      lineHeight: 1.375,
+    },
+    h6: {
+      fontWeight: 600,
+      fontSize: "1.125rem",
+      lineHeight: 1.375,
+    },
+  },
+});
